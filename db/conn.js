385eb6f7b67c8db568f22868f6c9244aad0ac191
hzzz <hzzz0823@gmail.com>
2018-04-27 13:31:53 +0800

    message save to db

---
+++ db/conn.js
@@ -0,0 +1,34 @@
+var mysql = require('mysql');
+var moment = require('moment');
+
+var conn = mysql.createConnection({
+    host: '35.189.178.51',
+    user: 'root',
+    password: 'hzzz',
+    database: 'hzzz'
+});
+
+module.exports = {
+
+    'createTable': () => {
+        var sql = 'CREATE TABLE IF NOT EXISTS MESSAGES (IP VARCHAR(20), NAME VARCHAR(20), MSG TEXT, AT TIMESTAMP)';
+        conn.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log('Table created');
+        });
+    },
+
+    'save': (ip, name, msg) => {
+        data = {
+            ip: ip,
+            name: name,
+            msg: msg,
+            at: moment().format("YYYY-MM-DD HH:mm:ss")
+        };
+        console.log(data);
+        conn.query('INSERT INTO MESSAGES set ?', data, (err, result) => {
+            if (err) throw err;
+            console.log('Message saved');
+        });
+    }
+};
